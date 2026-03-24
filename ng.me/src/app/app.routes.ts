aa1dd8b38e8af0b46ba9dd68d6a388b29ed245de
Alessio Ferrari <alessio.ferrari@marconirovereto.it>
2026-03-24 14:03:07 +0000

    Fix: routes

--- ng.me/src/app/app.routes.ts
+++ ng.me/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: Home},
     { path: '**', component: Home }
 ];
