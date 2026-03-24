55e71402e6ef863f61b44f1d1d62814d93c7123d
Alessio Ferrari <alessio.ferrari@marconirovereto.it>
2026-03-24 13:19:38 +0000

    Feat: add routes

--- ng.me/src/app/components/navbar/navbar.ts
+++ ng.me/src/app/components/navbar/navbar.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.html',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   styleUrl: './navbar.css',
